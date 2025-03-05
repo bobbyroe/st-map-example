a0f2080b5c7c28a6ef4bf89ecc61d4e8849f75ac
Bobby Roe <bobby.roe@unifyconsulting.com>
2025-03-05 10:56:29 -0800

    swap in new data

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pandas as pd
 import pydeck as pdk
 import numpy as np
 
-st.set_page_config(page_title="Contacts Dashboard", page_icon="🇺🇸", layout="wide")
+st.set_page_config(page_title="Map Example", page_icon="📍", layout="wide")
 
-st.title("Magenta Network Strength")
+st.title("Employees / Locations Map")
 
 st.logo(image="images/t-mobile-logo.png", 
         icon_image="images/t-logo.png")
@@ -18,16 +18,14 @@ greens = np.array([0, 164, 168])
 blues = np.array([116, 96, 224])
 @st.cache_data
 def load_data():
-    data = pd.read_csv('./data/WMCA.csv')
-    data = data.iloc[:, [3, 5, 6, 7, 36, 47, 58]] ## integer location based indexing
-    # City, Zip, lat, long, Avg Ookla Download Mbps, Avg Ookla Download Mbps - VZ, Avg Ookla Download Mbps - AT&T, 
-    data = data.dropna()
-    data["radius"] = data["Avg Ookla Download Mbps"] * 40
-    numeric_data = data.iloc[:, [4,5,6]].to_numpy()
-    max_index = np.argmax(numeric_data, axis=1)
-    data["r"] = reds[max_index]
-    data["g"] = greens[max_index]
-    data["b"] = blues[max_index]
+    data = pd.read_csv('./data/locations.csv')
+    # City, State, lat, long, SPEED
+    # data = data.dropna()
+    data["radius"] = data["EMPLOYEES_HERE"] * 400
+    
+    data["r"] = reds[0]
+    data["g"] = greens[0]
+    data["b"] = blues[0]
     return data
 
 data = load_data()
@@ -36,20 +34,20 @@ st.pydeck_chart(
     pdk.Deck(
         initial_view_state=pdk.ViewState(
             latitude=38.0,
-            longitude=-107.0,
-            zoom=3,
+            longitude=-100.0,
+            zoom=3.5,
             pitch=30,
         ),
         layers=[
             pdk.Layer(
                 "ScatterplotLayer",
                 data=data,
-                get_position=["longitude", "latitude"],
+                get_position=["LONGITUDE", "LATITUDE"],
                 get_line_color=[255, 255, 255],
                 get_fill_color=["r", "g", "b"],
                 get_radius="radius",
             )
         ],
-    #    tooltip={"text": "{object.Zip_agg}"}
+       tooltip={"text": "{EMPLOYEES_HERE}"}
     )
 )
